81aba6f5b79701869dbdf276abe149ad7c9c2d6c
Sunil Ballewad <sunilballewad@gmail.com>
2026-07-04 18:20:13 +0530

    Added login POST route

--- routes/auth.py
+++ routes/auth.py
@@ -1,7 +1,19 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 
 auth = Blueprint("auth", __name__)
 
 @auth.route("/")
-def login():
+def index():
     return render_template("login.html")
+
+
+@auth.route("/login", methods=["POST"])
+def login():
+
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    print("Email :", email)
+    print("Password :", password)
+
+    return redirect(url_for("dashboard"))
