0f14de5b77296fe3fd4c747d30534d126fd6fc98
Sunil Ballewad <sunilballewad@gmail.com>
2026-07-05 18:00:11 +0530

    Added ticket management and live dashboard

--- routes/dashboard.py
+++ routes/dashboard.py
@@ -1,7 +1,23 @@
 from flask import Blueprint, render_template
+from models import Ticket
 
 dashboard = Blueprint("dashboard", __name__)
 
 @dashboard.route("/dashboard")
 def dashboard_home():
-    return render_template("dashboard.html")
+
+    total = Ticket.query.count()
+    open_count = Ticket.query.filter_by(status="Open").count()
+    closed_count = Ticket.query.filter_by(status="Closed").count()
+    pending_count = Ticket.query.filter_by(status="Pending").count()
+
+    tickets = Ticket.query.order_by(Ticket.id.desc()).limit(5).all()
+
+    return render_template(
+        "dashboard.html",
+        total=total,
+        open_count=open_count,
+        closed_count=closed_count,
+        pending_count=pending_count,
+        tickets=tickets
+    )
--- routes/tickets.py
+++ routes/tickets.py
@@ -1,9 +1,8 @@
 from flask import Blueprint, render_template, request, redirect, url_for
-
 from models import db, Ticket
+import random
 
 tickets = Blueprint("tickets", __name__)
-
 
 @tickets.route("/tickets/create", methods=["GET", "POST"])
 def create_ticket():
@@ -11,11 +10,12 @@ def create_ticket():
     if request.method == "POST":
 
         ticket = Ticket(
-            ticket_number="INC0001",
+            ticket_number=f"INC{random.randint(10000,99999)}",
             title=request.form["title"],
             description=request.form["description"],
             category=request.form["category"],
             priority=request.form["priority"],
+            status="Open",
             created_by="Admin"
         )
 
@@ -30,9 +30,9 @@ def create_ticket():
 @tickets.route("/tickets")
 def view_tickets():
 
-    all_tickets = Ticket.query.all()
+    ticket_list = Ticket.query.order_by(Ticket.id.desc()).all()
 
     return render_template(
         "view_tickets.html",
-        tickets=all_tickets
+        tickets=ticket_list
     )
